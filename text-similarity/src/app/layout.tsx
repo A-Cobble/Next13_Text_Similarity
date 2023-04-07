6523af5d6024854a1b5b387631ee486336ec8d98
A-Cobble <alex.cobble42@gmail.com>
2023-04-07 11:28:11 -0700

    added an Icons file to the ui folder to handle the icons, added a Toast folder to the ui folder to catch errors when logging in. Added two dependencies for animations. Finished the light and dark mode toggle button in the navbar

--- text-similarity/src/app/layout.tsx
+++ text-similarity/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Providers from './components/Providers'
 import { cn } from '@/lib/utils'
 import Navbar from './components/Navbar'
+import { Toaster } from './components/ui/Toast'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -16,6 +17,8 @@ export default function RootLayout({
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialias'>
         <Providers>
           {children}
+
+          <Toaster position='bottom-right' />
 
           {/* @ts-expect-error Server Component */}
           <Navbar />
