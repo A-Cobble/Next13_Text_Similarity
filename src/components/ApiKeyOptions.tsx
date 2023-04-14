349963c4675a95af601a4785b7084c21a6e29412
A-Cobble <alex.cobble42@gmail.com>
2023-04-14 15:31:18 -0700

    added a method for the user to revoke their current API key.

--- src/components/ApiKeyOptions.tsx
+++ src/components/ApiKeyOptions.tsx
@@ -5,6 +5,9 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { Button } from '@/ui/Button';
 import { Loader2 } from 'lucide-react';
 import { toast } from './ui/Toast';
+import { useRouter } from "next/navigation"
+import { createApiKey } from '@/helpers/create-api-key';
+import { revokeApiKey } from '@/helpers/revoke-api-key';
 
 interface ApiKeyOptionsProps {
   apiKeyId: string,
@@ -14,6 +17,42 @@ interface ApiKeyOptionsProps {
 const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({apiKeyId, apiKeyKey}) => {
   const [isCreatingNew, setIsCreatingNew] = useState<boolean>(false);
   const [isRevoking, setIsRevoking] = useState<boolean>(false);
+  const router = useRouter();
+
+  const createNewApiKey = async () => {
+    setIsCreatingNew(true)
+
+    try{
+      await revokeApiKey({keyId: apiKeyId});
+      await createApiKey();
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: 'Error creating API key',
+        message: 'Please try again later.',
+        type: 'error',
+      })
+    } finally {
+      setIsCreatingNew(false)
+    }
+  }
+
+  const revokeCurrentApiKey = async () => {
+    setIsRevoking(true)
+
+    try{
+      await revokeApiKey({keyId: apiKeyId});
+      router.refresh();
+    }catch(error) {
+      toast({
+        title: 'Error revoking API key',
+        message: 'Please try again later.',
+        type: 'error',
+      })
+    }finally {
+      setIsRevoking(false)
+    }
+  }
 
   return (
     <DropdownMenu>
@@ -47,11 +86,11 @@ const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({apiKeyId, apiKeyKey}) => {
           Copy
         </DropdownMenuItem>
 
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={createNewApiKey}>
           Create new key
         </DropdownMenuItem>
 
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={revokeCurrentApiKey}>
           Revoke Key
         </DropdownMenuItem>
       </DropdownMenuContent>
